e793d0ac0d643aea3613326736e576c321056766
Fletcher Nichol <fnichol@nichol.ca>
2010-10-12 21:18:58 -0600

    Add json_attribs

--- solo.rb
+++ solo.rb
@@ -1,6 +1,7 @@
 # chef-solo -c /etc/chef/solo.rb -j /etc/chef/dna.json
 file_cache_path  "/tmp/chef-solo"
 cookbook_path    "/var/chef-solo/cookbooks"
+json_attribs     "/etc/chef/dna.json"
 log_level        :info
 log_location     STDOUT
 ssl_verify_mode  :verify_none
