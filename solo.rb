74265070ac3aab09d913a2a98711b04d302cd28a
Fletcher Nichol <fnichol@nichol.ca>
2010-11-28 20:18:01 -0700

    Add role_path.

--- solo.rb
+++ solo.rb
@@ -1,7 +1,8 @@
 # chef-solo -c /etc/chef/solo.rb -j /etc/chef/dna.json
-file_cache_path  "/tmp/chef-solo"
-cookbook_path    "/var/chef-solo/cookbooks"
-json_attribs     "/etc/chef/dna.json"
-log_level        :info
-log_location     STDOUT
-ssl_verify_mode  :verify_none
+file_cache_path   "/tmp/chef-solo"
+cookbook_path     "/var/chef-solo/cookbooks"
+roles_path        "/var/chef-solo/roles"
+json_attribs      "/etc/chef/dna.json"
+log_level         :info
+log_location      STDOUT
+ssl_verify_mode   :verify_none
