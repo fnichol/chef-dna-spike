2bbbc575f9f3992de90f6c3d354e2c8022d2af3d
Fletcher Nichol <fnichol@nichol.ca>
2010-11-28 20:19:52 -0700

    Update typo in role_path.

--- solo.rb
+++ solo.rb
@@ -1,7 +1,7 @@
 # chef-solo -c /etc/chef/solo.rb -j /etc/chef/dna.json
 file_cache_path   "/tmp/chef-solo"
 cookbook_path     "/var/chef-solo/cookbooks"
-roles_path        "/var/chef-solo/roles"
+role_path         "/var/chef-solo/roles"
 json_attribs      "/etc/chef/dna.json"
 log_level         :info
 log_location      STDOUT
